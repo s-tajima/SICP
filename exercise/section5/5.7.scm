ed453832e9983675d0bd364f6d172fd19c035985
s-tajima <tajima1989@gmail.com>
2016-10-11 12:54:07 +0900

    5.7

--- exercise/section5/5.7.scm
+++ exercise/section5/5.7.scm
@@ -25,3 +25,53 @@
 (print (set-register-contents! gcd-machine 'b 40))
 (print (start gcd-machine))
 (print (get-register-contents gcd-machine 'a))
+
+;; exercise 5.7 (section5.1)
+;; a
+(define expt-machine
+  (make-machine
+    '(b n val continue)
+    (list (list '= =) (list '- -) (list '* *))
+    '((assign continue (label expt-done))
+      expt-loop
+      (test (op =) (reg n) (const 0))
+      (branch (label return))
+      (save continue)
+      (assign n (op -) (reg n) (const 1))
+      (assign continue (label after-expt))
+      (goto (label expt-loop))
+      after-expt
+      (restore continue)
+      (assign val (op *) (reg b) (reg val))
+      (goto (reg continue))
+      return
+      (assign val (const 1))
+      (goto (reg continue))
+      expt-done)))
+
+(print (set-register-contents! expt-machine 'b 3))
+(print (set-register-contents! expt-machine 'n 4))
+(print (start expt-machine))
+(print (get-register-contents expt-machine 'val))
+;=> 81
+
+;; b
+(define expt-machine
+  (make-machine
+    '(counter product n b)
+    (list (list '= =) (list '* *) (list '- -))
+    '((assign counter (reg n))
+      (assign product (const 1))
+      expt-iter
+      (test (op =) (reg counter) (const 0))
+      (branch (label expt-done))
+      (assign product (op *) (reg b) (reg product))
+      (assign counter (op -) (reg counter) (const 1))
+      (goto (label expt-iter))
+      expt-done)))
+
+(print (set-register-contents! expt-machine 'b 3))
+(print (set-register-contents! expt-machine 'n 4))
+(print (start expt-machine))
+(print (get-register-contents expt-machine 'product))
+;=> 81
